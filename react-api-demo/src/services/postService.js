d736f41e66f34ee5d931719f7d72a17bd3e24777
Emin <eminperenda@gmail.com>
2026-06-11 11:48:35 +0200

    react api demo

---
+++ react-api-demo/src/services/postService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+const getPosts = () => {
+  return api.get("/posts");
+};
+
+const deletePost = (id) => {
+  return api.delete(`posts/${id}`);
+};
+
+const createPost = (post) => api.post("/posts", post);
+
+const updatePost = (id, post) => api.put(`/posts/${id}`, post);
+export { getPosts, deletePost, createPost, updatePost };
